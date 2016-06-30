2ec1923115dfb22a7256a68c98928881aafba9d9
Niklas Keller <me@kelunik.com>
2016-06-30 21:06:09 +0200

    Add base64 and base64url, change urlencode / urldecode to url sub-interpreter

--- js/app.js
+++ js/app.js
@@ -1,24 +1,18 @@
 $(".console").terminal({
-	rebecca: function () {
-		switch (new Date().getDay()) {
-			case 4:
-				this.echo("Happy Prebeccaday!");
-				break;
-			case 5:
-				window.location = "https://www.youtube.com/watch?v=kfVsfOSbJY0";
-				break;
-			case 6:
-				this.echo("Today is Saturday. And Sunday comes afterwards");
-				break;
-			default:
-				var now = new Date();
-				var then = new Date();
-				then.setDate(now.getDate() + (7 + 5 - now.getDay()) % 7);
-
-				var diff = then - now;
-
-				term.echo("Only " + diff.getDate() + " days, " + diff.getHours() + " hours and " + diff.getMinutes() + " minutes left until Rebeccaday, OMG!");
-				break;
+	base64: {
+		encode: function (str) {
+			this.echo(btoa(str));
+		},
+		decode: function (str) {
+			this.echo(atob(str));
+		}
+	},
+	base64url: {
+		encode: function (str) {
+			this.echo(btoa(str).replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, ""));
+		},
+		decode: function (str) {
+			this.echo(atob(str).replace(/-/g, "+").replace(/_/g, "/"));
 		}
 	},
 	blog: function () {
@@ -44,11 +38,35 @@ $(".console").terminal({
 	mail: function () {
 		this.echo("You can contact me (at) kelunik.com");
 	},
-	urlencode: function(str) {
-		this.echo(encodeURIComponent(str));
+	rebecca: function () {
+		switch (new Date().getDay()) {
+			case 4:
+				this.echo("Happy Prebeccaday!");
+				break;
+			case 5:
+				window.location = "https://www.youtube.com/watch?v=kfVsfOSbJY0";
+				break;
+			case 6:
+				this.echo("Today is Saturday. And Sunday comes afterwards");
+				break;
+			default:
+				var now = new Date();
+				var then = new Date();
+				then.setDate(now.getDate() + (7 + 5 - now.getDay()) % 7);
+
+				var diff = then - now;
+
+				term.echo("Only " + diff.getDate() + " days, " + diff.getHours() + " hours and " + diff.getMinutes() + " minutes left until Rebeccaday, OMG!");
+				break;
+		}
 	},
-	urldecode: function(str) {
-		this.echo(decodeURIComponent(str));
+	url: {
+		encode: function (str) {
+			this.echo(encodeURIComponent(str));
+		},
+		decode: function (str) {
+			this.echo(decodeURIComponent(str));
+		}
 	},
 	twitter: function () {
 		this.pause();
@@ -57,5 +75,5 @@ $(".console").terminal({
 }, {
 	greetings: "Welcome on kelunik.com\n\nJust type what you're looking for. Tab completion is available.\n",
 	name: "kelunik.com",
-	prompt: "kelunik.com $ "
+	prompt: "	$ "
 });
